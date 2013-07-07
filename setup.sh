bdbce81d49d8f3933d6ac7cd41803ffe9473229b
Vytautas Astrauskas <vastrauskas@gmail.com>
2013-07-07 15:30:09 +0300

    Added personal config.

--- setup.sh
+++ setup.sh
@@ -26,6 +26,9 @@ sudo apt-add-repository -y ppa:cassou/emacs
 sudo apt-get update
 sudo apt-get install -y emacs24 emacs24-el emacs24-common-non-dfsg
 
+# Install tmux.
+sudo apt-get install -y tmux
+
 # git pull and install dotfiles as well
 cd $HOME
 if [ -d ./dotfiles/ ]; then
@@ -34,10 +37,24 @@ fi
 if [ -d .emacs.d/ ]; then
     mv .emacs.d .emacs.d~
 fi
-git clone https://github.com/startup-class/dotfiles.git
-ln -sb dotfiles/.screenrc .
+git clone https://github.com/vakaras/ec2-dotfiles.git
+ln -sb dotfiles/.tmux.conf .
 ln -sb dotfiles/.bash_profile .
 ln -sb dotfiles/.bashrc .
 ln -sb dotfiles/.bashrc_custom .
-ln -sf dotfiles/.emacs.d .
+ln -sb dotfiles/.vimrc
+#ln -sf dotfiles/.emacs.d .
 
+# Install heroku.
+wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+
+# Set up heroku
+heroku login
+ssh-keygen -t rsa
+heroku keys:add
+
+# Init bitstarter project.
+git clone https://github.com/vakaras/bitstarter.git
+cd bitstarter
+heroku create
+git push heroku master
